230a448d9d5a4712668323544e9f4ef878377663
PEDRO Maia <pedromaia@MBP-de-PEDRO.lan>
2021-12-20 07:08:17 +0000

    change some things

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ export default function Topbar({menuOpen, setMenuOpen}) {
         <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" className="logo">genius.</a>
+                    <a href="#intro" className="logo">JD PH</a>
                     <div className="itemContainer">
                         <Person className="icon"/>
                         <span>+351 914 955 967</span>
